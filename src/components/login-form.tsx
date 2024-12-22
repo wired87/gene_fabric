684ca40460a1649521c41a7296d64f506e8e8575
wired87 <derbenedikt.sterra@gmail.com>
2024-12-22 09:01:39 +0100

    pnpm

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -6,7 +6,7 @@ import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useRouter } from "next/navigation"
-import { useState } from "react";
+import React, { useState } from "react";
 // import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 
 export function LoginForm({
