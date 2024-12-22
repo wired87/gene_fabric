2bcb21d4a59f7cfde5f5154827a05ec5cb510c23
wired87 <derbenedikt.sterra@gmail.com>
2024-12-22 09:36:59 +0100

    removed stuff

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,6 @@
-import FunFact from "@/components/FunFact";
-import {factDataHome} from "@/components/FunFact/factData";
-import Feature from "@/components/Features";
-import {homeFeaturesData} from "@/components/Features/featuresData";
-import CTA from "@/components/CTA";
-import Integration from "@/components/Integration";
-import Contact from "@/components/Contact";
+
 import React from "react";
-import HomeBot from "@/components/ChatBot/HomeBot";
+
 const headerInfo= {
   title: "Wo sind wir die besten",
   subtitle: "Unsere Spezialgebiete",
@@ -14,19 +8,21 @@ const headerInfo= {
   Durch unsere Expertise und Erfahrung decken wir ein breites Spektrum an Möglichkeiten ab, um Ihr Projekt optimal umzusetzen.`,
 }
 
-
+/*
+ <FunFact data={factDataHome}/>
+<Feature data={homeFeaturesData} headerInfo={headerInfo}/>
+<CTA />
+<Integration />
+<HomeBot />
+<Contact />
+ */
 const HomeMain = () => {
 
 
 
   return(
     <div className={"w-full"}>
-      <FunFact data={factDataHome}/>
-      <Feature data={homeFeaturesData} headerInfo={headerInfo}/>
-      <CTA />
-      <Integration />
-      <HomeBot />
-      <Contact />
+
     </div>
   );
 }
