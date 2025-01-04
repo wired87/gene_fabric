b7ab818576f0c76f4c85da2cc1134818f5639fc1
Prateek Rohilla <prateekrohilla4.pr@gmail.com>
2025-01-04 03:31:22 +0100

    Add Slider component and integrate it into the Home page; update package.json and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Head from "./(site)/head";
+import { Slider } from "@/components/ui/slider";
 
 export default function Home() {
-  return (<>
-    <Head />
-    {/* 4 c */}
-  </>
+  return (
+    <>
+      <Head />
+      <div className="container mx-auto px-4 py-8 w-96">
+        <h1 className="text-3xl mb-8">Gene fabric</h1>
+        <h3 className="text-lg">Target Protein Sequence</h3>
+
+        <h3 className="text-lg">Stability</h3>
+        <div className="container my-2">
+          <Slider defaultValue={[33]} max={100} step={1} className="pb-2" />
+        </div>
+        <h3 className="text-lg">Activity</h3>
+        <div className="container my-2">
+          <Slider defaultValue={[33]} max={100} step={1} />
+        </div>
+      </div>
+
+      {/* 4 c */}
+    </>
   );
 }
